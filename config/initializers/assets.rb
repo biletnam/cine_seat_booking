48a42539a2f9a4befcebda0128bada0319f87862
tarique313 <mahmud.tarique@gmail.com>
2016-08-29 14:35:01 +0600

    Styles added

---
+++ config/initializers/assets.rb
@@ -0,0 +1,20 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile << /\.(?:png|jpg|jpeg|gif)\z/
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( normalize.css )
+Rails.application.config.assets.precompile += %w( demo.css )
+Rails.application.config.assets.precompile += %w( component.css )
+Rails.application.config.assets.precompile += %w( modernizr-custom.js)
+Rails.application.config.assets.precompile += %w( classie.js )
+Rails.application.config.assets.precompile += %w( main.js )
+#Rails.application.config.assets.paths << "#{Rails.root}/app/assets/videos"
+
