0226a5c9bbf20cb4921e1b9ddc14b7331b412112
tarique313 <mahmud.tarique@gmail.com>
2016-08-31 12:27:16 +0600

    Reservation model

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,4 +1,27 @@
 class ReservationsController < ApplicationController
+  
   def index
+  	a=[true, false]
+  	#a=['row__seat tooltip','row__seat row__seat--reserved']
+  	@b= []
+  	a.cycle(9){|x| @b.append(x)}
+
   end
+
+  def create
+
+  end
+
+  def show
+  	@reservation = Reservation.find(params[:id])	
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+
 end
